b867902ab8519dc3cc1fcc8720f6fc6e0f64ede4
Ashvin-Pal <ashvinshash@gmail.com>
2023-12-03 00:26:22 +0100

    build: prepare for alpha publish

---
+++ README.md
@@ -0,0 +1,1 @@
+# jest-console-group-reporter
--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,5 @@ module.exports = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
-  reporters: ["./dist/index.cjs"],
+  reporters: ["./dist/index.js"],
 };
