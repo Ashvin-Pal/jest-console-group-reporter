cae9d6bda9f7890049d00ea61b530f6ac6113356
Ashvin-Pal <ashvinshash@gmail.com>
2023-12-04 13:37:08 +0100

    test: add unit test for shouldFilterMessage

---
+++ src/__test__/shouldFilterMessage.spec.ts
@@ -0,0 +1,48 @@
+import { shouldFilterMessage } from "../shouldFilterMessage";
+import type { ConsoleMessage } from "@/types";
+
+const errorConsoleMessage = { message: "error", type: "error", origin: "console" };
+const errorTypePredicateFn = ({ type }: ConsoleMessage): boolean => type === "error";
+const messageTypePredicateFn = ({ message }: ConsoleMessage): boolean => /^error/.test(message);
+const originTypePredicateFn = ({ origin }: ConsoleMessage): boolean => /^console/.test(origin);
+
+describe("shouldFilterMessage", () => {
+  test.each`
+    filters              | consoleMessage         | expectedOutput
+    ${["error"]}         | ${errorConsoleMessage} | ${true}
+    ${["warn"]}          | ${errorConsoleMessage} | ${false}
+    ${["warn", "error"]} | ${errorConsoleMessage} | ${true}
+  `(
+    "filters out messages correctly when matching a string filter",
+    ({ filters, consoleMessage, expectedOutput }) => {
+      expect(shouldFilterMessage(filters, consoleMessage)).toBe(expectedOutput);
+    }
+  );
+
+  test.each`
+    filters              | consoleMessage         | expectedOutput
+    ${[/error/]}         | ${errorConsoleMessage} | ${true}
+    ${[/warn/]}          | ${errorConsoleMessage} | ${false}
+    ${[/warn/, /error/]} | ${errorConsoleMessage} | ${true}
+  `(
+    "filters out messages correctly when matching a regex expression",
+    ({ filters, consoleMessage, expectedOutput }) => {
+      expect(shouldFilterMessage(filters, consoleMessage)).toBe(expectedOutput);
+    }
+  );
+
+  test.each`
+    filters                                          | consoleMessage                                                     | expectedOutput
+    ${[errorTypePredicateFn]}                        | ${{ message: "error message", type: "error", origin: "console" }}  | ${true}
+    ${[messageTypePredicateFn]}                      | ${{ message: "error message", type: "warn", origin: "console" }}   | ${true}
+    ${[errorTypePredicateFn, originTypePredicateFn]} | ${{ message: "error message", type: "error", origin: "console" }}  | ${true}
+    ${[errorTypePredicateFn]}                        | ${{ message: "info message", type: "warn", origin: "console" }}    | ${false}
+    ${[messageTypePredicateFn]}                      | ${{ message: "must not match", type: "error", origin: "console" }} | ${false}
+    ${[errorTypePredicateFn, originTypePredicateFn]} | ${{ message: "error message", type: "warn", origin: "nowhere" }}   | ${false}
+  `(
+    "filters out messages correctly when using a predicate function",
+    ({ filters, consoleMessage, expectedOutput }) => {
+      expect(shouldFilterMessage(filters, consoleMessage)).toBe(expectedOutput);
+    }
+  );
+});
